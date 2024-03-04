722396c3a515b1d2e85556bdc14bbba9b32bce0c
bocsibarna <bocsibarna@gmail.com>
2024-03-04 16:48:43 +0100

    testz2

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,44 @@
-package hu.bme.mit.train.sensor;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
-public class TrainSensorTest {
+class TrainSensorImplTest {
 
-    @Before
-    public void before() {
-        // TODO Add initializations
+    private TrainController mockController;
+    private TrainUser mockUser;
+    private TrainSensorImpl trainSensor;
+
+    @BeforeEach
+    void setUp() {
+        mockController = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockController, mockUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    void getSpeedLimit_shouldReturnInitialSpeedLimit() {
+        // Arrange
+        int expectedSpeedLimit = 5;
+
+        // Act
+        int actualSpeedLimit = trainSensor.getSpeedLimit();
+
+        // Assert
+        assertEquals(expectedSpeedLimit, actualSpeedLimit);
     }
+
+    @Test
+    void overrideSpeedLimit_shouldUpdateSpeedLimitAndCallController() {
+        // Arrange
+        int newSpeedLimit = 10;
+
+        // Act
+        trainSensor.overrideSpeedLimit(newSpeedLimit);
+
+        // Assert
+        assertEquals(newSpeedLimit, trainSensor.getSpeedLimit());
+        verify(mockController, times(1)).setSpeedLimit(newSpeedLimit);
+    }
+
+    // Additional tests can be added here if needed
 }
